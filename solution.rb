4ce24dfe637f80654b825d296cdc0775a9be37fc
Mark Del Mar <mark@1000martians.com>
2019-09-09 10:05:52 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,34 @@
 class Deck
 
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    rank.each do
+      suit.each do
+        @cards << Card.new(suit, rank)
+      end
+    end
+  end
+
+  def choose_card
+    cards.delete_at(rand(1..52))
+  end
+
 end
 
+
+
 class Card
+
+  attr_accessor :suit, :rank
+
+  def initialize(suit, rank)
+    @rank = rank
+    @suit = suit
+  end
 
 end
